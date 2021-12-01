ff2a5ddfbdcb581d2eb10a5e7986756b92715f9a
JSotomayr <jaime.sotomayor.rodriguez@gmail.com>
2021-12-01 11:23:58 +0000

    first try

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,27 +1,25 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import injectContext from "./store/appContext";
 
 import { Home } from "./pages/home";
+
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
-import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-//create your first component
+
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	
 	const basename = process.env.BASENAME || "";
 
 	return (
 		<div>
 			<BrowserRouter basename={basename}>
-				<ScrollToTop>
 					<Navbar />
-					<Switch>
+					<Routes>
 						<Route exact path="/">
 							<Home />
 						</Route>
@@ -34,9 +32,8 @@ const Layout = () => {
 						<Route>
 							<h1>Not found!</h1>
 						</Route>
-					</Switch>
+					</Routes>
 					<Footer />
-				</ScrollToTop>
 			</BrowserRouter>
 		</div>
 	);
