ddc41dac966988c5f856c3ff5b65f2ebdfa764c6
Carolth7 <carol.cth93@gmail.com>
2021-12-06 16:34:00 +0000

    guardando post user y post login en routes, models, y app.py

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -18,6 +18,11 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
 
 
     @classmethod
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,19 +4,50 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 import os
 from datetime import timedelta, datetime
 
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, Blueprint
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 
 
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
-from utils import APIException, generate_sitemap
-from admin import setup_admin
+from api.utils import APIException, generate_sitemap
+from api.admin import setup_admin
 from sqlalchemy import exc
-from models import db, User, 
+from api.models import db, User
+from werkzeug.security import check_password_hash, generate_password_hash
 
 api = Blueprint('api', __name__)
+
+
+@api.route('/user', methods=['POST'])
+def create_user(): 
+
+    is_active = True
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    username = request.json.get("username", None)
+
+    if not (email and password and username):
+        return {"error":"Missing info"}, 400
+
+    new_user = User(
+        is_active = is_active,
+        email=email,
+        password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16),
+        username=username
+    )
+
+    try:
+        new_user.create()
+    except exc.IntegrityError: 
+        return {"error":"something went wrong"}, 409
+
+
+    if new_user:
+        token = create_access_token(identity=new_user.serialize(), expires_delta=timedelta(minutes=100))
+        return({'token' : token}), 200
+
 
 
 @api.route('/login', methods=["POST"])
@@ -30,7 +61,7 @@ def login():
 
     user = User.get_by_email(email)
 
-    if user and check_password_hash(user._password, password) and user._is_active:
+    if user and check_password_hash(user.password, password) and user.is_active:
         token = create_access_token(identity=user.serialize(), expires_delta=timedelta(minutes=100))
         return({'token' : token}) , 200
 
