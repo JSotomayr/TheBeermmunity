eff871b20ac29fdd807832b5834ab73a03910589
AnnaLemons <a.lopezdelg@gmail.com>
2021-12-06 16:41:11 +0000

    saved

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,17 +8,6 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend"
-    }
-
-    return jsonify(response_body), 200
-
-
-
 @app.route('/login', methods=['POST'])
 def login():
     email = request.json.get('email', None)
