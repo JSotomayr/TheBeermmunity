4f62a0716526b3cad4e9937cadaf8f8c68440afc
Carolth7 <carol.cth93@gmail.com>
2021-12-08 14:59:46 +0000

    añadiendo tamaño letra

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,14 +2,13 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.scss";
 import { Link } from "react-router-dom";
-import { Navbar } from "../component/navbar";
+
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center">
-			<Navbar/>
 			<div className="btn mt-5">
 				<Link to="/login">
 					LOGIN
