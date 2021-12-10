989b2556fcb9bc7741ff3157cf5c70a4cb1bab18
Jaime Sotomayor <89789808+JSotomayr@users.noreply.github.com>
2021-12-10 17:16:40 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -11,11 +11,16 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 #from models import Person
+from flask_jwt_extended import JWTManager
 
 ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET_KEY')
+jwt = JWTManager(app)
+
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
