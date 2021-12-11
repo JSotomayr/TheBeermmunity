ca634bb709c0a09db78f240618cc6e429da7e884
Carolth7 <carol.cth93@gmail.com>
2021-12-11 12:36:41 +0000

    perfeccionando formulario

--- src/front/js/pages/formBusiness.jsx
+++ src/front/js/pages/formBusiness.jsx
@@ -15,48 +15,83 @@ import { useForm, Controller } from "react-hook-form";
 	useEffect(() => {
 		setRegisterForm(
 			<Fragment>
-				<label htmlFor="username"></label>
-				<input 
-					id ="username"
-					placeholder="Nombre y apellidos"
-					className="form-control"
-					aria-invalid={errors.username ? "true" : "false"}
-					{...register("username", { required: true, maxLength: 30})}
-				/>
-					<div className="alertDiv">
-							{errors.username && errors.username.type === "required" && <span role="alert">El campo Usuario es obligatorio</span>}
-							{errors.username && errors.username.type === "maxLength" && <span role="alert">Máximo de caracteres excedido</span>}
+				 <div className="loginContainer">
+					<div className="btn nearLog">
+						{/* <Link to="/">
+							volver
+						</Link> */}
 					</div>
-				<input placeholder ="Name"
-				{...register("firstName", {required: true, maxLength: 20})}
-				/>
-				<input placeholder ="LastName"
-				{...register("LastName", {required: true, maxLength: 20})}
-				/>
-				<label>Admin</label>
-				<input
-					type="radio"
-					value="admin"
-					name="userType"
-					{...register("userType")}
-				/>
-				<label>Business</label>
-				<input
-					type="radio"
-					value="business"
-					name="userType"
-					{...register("userType")}
-				/>
-				<button 
-				onClick ={ (event) => {
-					event.preventDefault();
-					if (getValues("userType") != null){
-						setNext(true);
-						}
-					}}
-				>
-					Next
-				</button>
+					<p>
+						{/* <img src={logoBig} /> */}
+					</p>
+					<div className="title">Crear cuenta</div>
+					<div className="login-form">
+						<label htmlFor="username"></label>
+						<input 
+							id ="username"
+							placeholder="Nombre de usuario"
+							className="form-control"
+							aria-invalid={errors.username ? "true" : "false"}
+							{...register("username", { required: true, maxLength: 30, })}
+						/>
+							<div className="alertDiv">
+									{errors.username && errors.username.type === "required" && <span role="alert">El campo Usuario es obligatorio</span>}
+									{errors.username && errors.username.type === "maxLength" && <span role="alert">Máximo de caracteres excedido</span>}
+							</div>
+							<label htmlFor="username"></label>
+						<input 
+							id ="Email"
+							placeholder="Email"
+							className="form-control"
+							aria-invalid={errors.Email ? "true" : "false"}
+							{...register("Email", { required: true, maxLength: 30, pattern: {value: /\S+@\S+\.\S+/ }})}
+						/>
+							<div className="alertDiv">
+									{errors.Email && errors.Email.type === "required" && <span role="alert">El campo correo es obligatorio</span>}
+									{errors.Email && errors.Email.type === "maxLength" && <span role="alert">Máximo de caracteres excedido</span>}
+									{errors.Email && errors.Email.type === "pattern" && <span role="alert">Correo inválido</span>}
+							</div>
+						<label htmlFor="password"></label>
+ 						 <input 
+							 id ="password"
+							 type="password"
+							 placeholder="Contraseña"
+							 className="form-control"
+							 aria-invalid={errors.password ? "true" : "false"}
+							 {...register("password", { required: true, minLength: 5})}
+							 />
+						 <div className="alertDiv">
+							 {errors.password && errors.password.type === "required" && <span role="alert">El campo Contraseña es obligatorio</span>}
+							 {errors.password && errors.password.type === "minLength" && <span role="alert">Mínimo de longitud es de 5 caracteres</span>}
+						 </div>
+
+
+						<label>Admin</label>
+						<input
+							type="radio"
+							value="admin"
+							name="userType"
+							{...register("userType")}
+						/>
+						<label>Business</label>
+						<input
+							type="radio"
+							value="business"
+							name="userType"
+							{...register("userType")}
+						/>
+						<button 
+						onClick ={ (event) => {
+							event.preventDefault();
+							if (getValues("userType") != null){
+								setNext(true);
+								}
+							}}
+						>
+							Next
+						</button>
+					</div>
+				</div>
 			</Fragment>
 		);
 	}, []);
