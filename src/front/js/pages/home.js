73ffc60daeb2da443f44083c4aaef3e1cd1d1bd4
AnnaLemons <a.lopezdelg@gmail.com>
2021-12-14 17:22:25 +0000

    Merge

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,41 +8,6 @@ import { Footer } from "../component/footer";
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
-<<<<<<< HEAD
-	return (
-		<Fragment>
-
-			<div className="text-center">
-				<Navbar/>
-				<div className="btn mt-5">
-					<Link to="/login">
-						LOGIN
-					</Link>	
-				</div>
-				<div className="btn mt-5">
-					<Link to="/register">
-						REGISTER
-					</Link>	
-				</div>
-				<div>
-					<Link to={"/beer"}>
-						<h1>CERVEZAS</h1>
-					</Link>
-				</div>
-				<div>
-				<Link to={"/beer"}>
-					<h1>ENSEÑAME LA BIRRA</h1>
-				</Link>
-				</div>
-				<div>
-				<Footer />
-				</div>
-
-			</div>
-		</Fragment>
-	);
-};
-=======
   return (
     <Fragment>
       <div className="text-center">
@@ -63,8 +28,10 @@ export const Home = () => {
             <h1>ENSEÑAME LA BIRRA</h1>
           </Link>
         </div>
+        <div>
+          <Footer />
+        </div>
       </div>
     </Fragment>
   );
 };
->>>>>>> main
