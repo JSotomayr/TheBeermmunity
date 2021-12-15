a3ff1f087498bbbbddbccc37b702d1b1aa3ceeee
AnnaLemons <a.lopezdelg@gmail.com>
2021-12-15 11:29:15 +0000

    pulling main finally

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,75 @@
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const dotenv = require("dotenv");
 
-module.exports = merge(common, {
-  mode: "production",
+dotenv.config();
+
+module.exports = {
+  entry: ["babel-polyfill", "./src/front/js/index.js"],
   output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "public"),
     publicPath: "/",
   },
-});
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          {
+            loader: "style-loader", // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader", // compiles Sass to CSS
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: { name: "[name].[ext]" },
+        },
+      }, //for images
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        use: ["file-loader"],
+      }, //for fonts
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      Popper: "popper.js",
+      jQuery: "jquery",
+      // In case you imported plugins individually, you must also require them here:
+      Util: "exports-loader?Util!bootstrap/js/dist/util",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+    }),
+    new HtmlWebpackPlugin({
+      favicon: "4geeks.ico",
+      template: "template.html",
+    }),
+  ],
+};
