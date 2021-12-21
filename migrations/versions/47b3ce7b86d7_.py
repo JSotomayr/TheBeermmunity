65ecd5627521bbec715055b05a05211222a617ef
JSotomayr <jaime.sotomayor.rodriguez@gmail.com>
2021-12-21 17:21:12 +0000

    making changes

--- migrations/versions/47b3ce7b86d7_.py
+++ migrations/versions/47b3ce7b86d7_.py
@@ -1,20 +1,8 @@
 """empty message
 
-<<<<<<< HEAD:migrations/versions/4afbe29c4176_.py
-<<<<<<< HEAD:migrations/versions/4afbe29c4176_.py
-Revision ID: 4afbe29c4176
+Revision ID: 47b3ce7b86d7
 Revises: 
-Create Date: 2021-12-16 11:36:04.212445
-=======
-Revision ID: 7c2060add899
-Revises: 
-Create Date: 2021-12-16 10:39:37.390464
->>>>>>> 7848b54cadd59c4ff945d21309cfdae612625859:migrations/versions/7c2060add899_.py
-=======
-Revision ID: 7c2060add899
-Revises: 
-Create Date: 2021-12-16 10:39:37.390464
->>>>>>> main:migrations/versions/7c2060add899_.py
+Create Date: 2021-12-21 17:13:36.534552
 
 """
 from alembic import op
@@ -22,15 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-<<<<<<< HEAD:migrations/versions/4afbe29c4176_.py
-<<<<<<< HEAD:migrations/versions/4afbe29c4176_.py
-revision = '4afbe29c4176'
-=======
-revision = '7c2060add899'
->>>>>>> 7848b54cadd59c4ff945d21309cfdae612625859:migrations/versions/7c2060add899_.py
-=======
-revision = '7c2060add899'
->>>>>>> main:migrations/versions/7c2060add899_.py
+revision = '47b3ce7b86d7'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -82,6 +62,8 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('company_name', sa.String(), nullable=False),
     sa.Column('address', sa.String(), nullable=False),
+    sa.Column('latitude', sa.String(), nullable=True),
+    sa.Column('longitude', sa.String(), nullable=True),
     sa.Column('id_customer', sa.Integer(), nullable=False),
     sa.ForeignKeyConstraint(['id_customer'], ['customer.id'], ),
     sa.PrimaryKeyConstraint('id'),
